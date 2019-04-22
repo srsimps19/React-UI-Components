f8f39e2ec26e52376cc7714cf33372d798ff23ad
Sydney Crumley <srsimps19@gmail.com>
2019-04-22 14:30:08 -0500

    added JSX to all header components

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,15 @@
 import React from 'react';
-import './Header.css';
+import './Header.scss';
+import moment from 'moment';
+
+const HeaderTitle = () => {
+    return (
+        <div className="header">
+            <p>Lambda School</p>
+            <p>@LambdaSchool</p>
+            <p>{moment().format('MMMM Do YYYY, h:mm:ss a')}</p>
+        </div>
+    );
+};
+
+export default HeaderTitle;
