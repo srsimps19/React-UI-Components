c4756d993982c6ecd98c27d267ce41b504adb7e4
Sydney Crumley <srsimps19@gmail.com>
2019-04-23 15:48:16 -0500

    added JSX and SCSS to buttons and added buttons to app and added app styling

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import './App.scss';
 import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import NumberButton from './components/ButtonComponents/NumberButton';
+import ActionButton from './components/ButtonComponents/ActionButton';
 
 const App = () => {
   return (
-    <CalculatorDisplay />
-     
-    
+    <div className="app">
+      <CalculatorDisplay />
+      <div className="all-btns">
+        <div className="white-btns">
+          <ActionButton text="clear" btnStyle="white-large-btn" />
+          <div className="number-btn">
+            <NumberButton text="7" />
+            <NumberButton text="8" />
+            <NumberButton text="9" />
+            <NumberButton text="4" />
+            <NumberButton text="5" />
+            <NumberButton text="6" />
+            <NumberButton text="1" />
+            <NumberButton text="2" />
+            <NumberButton text="3" />
+            </div>
+          <ActionButton text="0" btnStyle="white-large-btn" />
+        </div>
+        <div className="red-btns">
+          <NumberButton text="÷" buttonStyle="red-btn" />
+          <NumberButton text="x" buttonStyle="red-btn" />
+          <NumberButton text="-" buttonStyle="red-btn" />
+          <NumberButton text="+" buttonStyle="red-btn" />
+          <NumberButton text="=" buttonStyle="red-btn" />
+        </div>
+      </div>
+    </div>
   );
 };
 
--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,10 @@
 import React from 'react';
-import './Button.css';
+import './Button.scss';
+
+const ActionButton = action => {
+    return (
+        <button className={action.btnStyle}>{action.text}</button>
+    );
+};
+
+export default ActionButton;
